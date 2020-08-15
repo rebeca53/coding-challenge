c1b0002ab8af046c5672dd3de2f49794faabc081
rebeca53 <rebeca.n.rod@gmail.com>
2020-08-15 19:39:53 -0400

    search view working

--- SearchApplication/app/src/main/java/com/example/searchapplication/MainActivity.java
+++ SearchApplication/app/src/main/java/com/example/searchapplication/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import java.util.LinkedList;
 
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private final LinkedList<String> m_wordList = new LinkedList<>();
     private WordListAdapter m_adapter;
     private RecyclerView m_recyclerView;
+    private SearchView m_searchView;
 
     private void populateWordList()
     {
@@ -23,16 +30,73 @@ public class MainActivity extends AppCompatActivity {
         m_wordList.addLast("pale");
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "Create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        populateWordList();
 
         m_recyclerView = findViewById(R.id.recyclerview);
         m_adapter = new WordListAdapter(this, m_wordList);
         m_recyclerView.setAdapter(m_adapter);
         m_recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        populateWordList();
+        Log.d("MainActivity", "CreateFinished");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("MainActivity", "Create menu");
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        m_searchView = (SearchView) menu.findItem(R.id.app_bar_search)
+                .getActionView();
+        m_searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        m_searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        m_searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                m_adapter.getFilter().filter(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                m_adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        Log.d("MainActivity", "Create menu finished");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.app_bar_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // close search view on back button pressed
+        if (!m_searchView.isIconified()) {
+            m_searchView.setIconified(true);
+            return;
+        }
+        super.onBackPressed();
+    }
+
 }
--- SearchApplication/app/src/main/java/com/example/searchapplication/WordListAdapter.java
+++ SearchApplication/app/src/main/java/com/example/searchapplication/WordListAdapter.java
@@ -1,9 +1,12 @@
 package com.example.searchapplication;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,17 +14,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.LinkedList;
 
-public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> implements Filterable {
     private final LinkedList<String> m_wordList;
+    private LinkedList<String> m_filteredList;
 
     private LayoutInflater m_inflater;
 
     public WordListAdapter(Context context, LinkedList<String> m_wordList) {
         m_inflater = LayoutInflater.from(context);
         this.m_wordList = m_wordList;
+        this.m_filteredList = m_wordList;
     }
 
-    class WordViewHolder extends RecyclerView.ViewHolder {
+    class WordViewHolder extends RecyclerView.ViewHolder{
         public final TextView wordItemView;
         final WordListAdapter m_wordListAdapter;
 
@@ -30,7 +35,6 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
             wordItemView = itemView.findViewById(R.id.textView);
             this.m_wordListAdapter = adapter;
         }
-
     }
 
     @NonNull
@@ -42,12 +46,158 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
 
     @Override
     public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
-        String m_current = m_wordList.get(position);
+        String m_current = m_filteredList.get(position);
         holder.wordItemView.setText(m_current);
     }
 
     @Override
     public int getItemCount() {
-        return m_wordList.size();
+        if (m_filteredList == null)
+        {
+            return 0;
+        }
+        return m_filteredList.size();
     }
+
+    boolean isPartialPermutation(String wordA, String wordB){
+        int lettersChanged = 0;
+        int counterA = 0;
+        int counterB = 0;
+
+        for (char a : wordA.toCharArray())
+        {
+            counterB = 0;
+            for (char b : wordB.toCharArray())
+            {
+                if (counterA == counterB){
+                    if (counterA == 0 && a != b){
+                        return false;
+                    }
+                    if (a != b)
+                    {
+                        lettersChanged++;
+                    }
+                }
+
+                if (b == a){
+                    wordB = wordB.substring(0, counterB)+'$' + wordB.substring(counterB+1);
+                }
+
+                counterB++;
+            }
+            counterA++;
+        }
+
+        if (lettersChanged/counterA < (2/3))
+        {
+            return false;
+        }
+
+        for (char b : wordB.toCharArray())
+        {
+            if (b != '$'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    boolean isMissingChar(String wordA, String wordB)
+    {
+        if (wordA.length() < wordB.length()){
+            return false;
+        }
+        for (int i=0; i<wordA.length()-1;i++){
+            if (wordA.charAt(i) != wordB.charAt(i))
+            {
+                return wordA.charAt(i+1) == wordB.charAt(i);
+            }
+        }
+
+        return true;
+    }
+
+    boolean isCharInserted(String wordA, String wordB){
+        if (wordB.length() < wordA.length()){
+            return false;
+        }
+
+        for (int i=0; i<wordB.length()-1; i++){
+            if (wordA.charAt(i) != wordB.charAt(i)){
+                return wordA.charAt(i) == wordB.charAt(i+1);
+            }
+        }
+
+        return false;
+    }
+
+    boolean isCharReplaced(String wordA, String wordB){
+        if (wordA.length() != wordB.length()){
+            return false;
+        }
+
+        int replacedQuantity = 0;
+        for (int i =0; i<wordA.length() -1; i++){
+            if (wordA.charAt(i) != wordB.charAt(i)){
+                replacedQuantity++;
+            }
+
+            if (replacedQuantity > 1){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    boolean isTypo(String wordA, String wordB){
+        return isMissingChar(wordA,wordB) || isCharInserted(wordA,wordB) || isCharReplaced(wordA,wordB);
+    }
+
+    boolean isTypoORisPermutation(String wordA, String wordB){
+        if (isTypo(wordA, wordB) && isPartialPermutation(wordA, wordB))
+            return false;
+
+        return isTypo(wordA, wordB) || isPartialPermutation(wordA, wordB);
+    }
+    @Override
+    public Filter getFilter() {
+        Log.d("WordListAdapter", "getFilter");
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    Log.d("WordListAdapter", "empty string");
+                    m_filteredList = m_wordList;
+                } else {
+                    LinkedList<String> filteredList = new LinkedList<String>();
+                    for (String row : m_wordList) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.contains(charSequence)) {
+                            filteredList.add(row);
+                        }
+                    }
+                    Log.d("WordListAdapter", "not empty string");
+                    m_filteredList = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = m_filteredList;
+                Log.d("WordListAdapter", "filter done");
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                Log.d("WordListAdapter", "publish");
+                m_filteredList = (LinkedList<String>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
 }
